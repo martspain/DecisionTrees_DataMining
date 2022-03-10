0dd4d8228bdbbe5fcb1d96d6982bb145ac1d7963
srue1834 <53914141+srue1834@users.noreply.github.com>
2022-03-09 18:01:23 -0600

    decision tree :(
    no funciona lolz

--- src/decision.py
+++ src/decision.py
@@ -1,25 +1,20 @@
+from locale import normalize
 import random
+from re import X
+from tkinter import Y
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 import sklearn
-# import matplotlib.pyplot as plt
-# from sklearn import datasets
-# import seaborn as sb
-# import sklearn.cluster as cluster
-# import sklearn.metrics as metrics
-# import sklearn.preprocessing
-# import scipy.cluster.hierarchy as sch
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 from fcmeans import FCM
-# import skfuzzy as fuzz
-# import pylab
-# import sklearn.mixture as mixture
 import pyclustertend
 from sklearn import cluster 
-# import random
-# import matplotlib.cm as cm
 from reader import Reader
+
+
+
 
 class main(object):
     def __init__(self, csvFilePath):
@@ -28,38 +23,34 @@ class main(object):
         # Classes
         R = Reader(csvFilePath)
         self.df = R.data
-    
-    def exploreData(self):
-        df = self.df
-        print('-------------------------------------')
-        for elem in df:
-            space = ''
-            if len(elem) < 15:
-                count = 15 - len(elem)
-            else:
-                count = 0
-            for n in range(count):
-                space += ' '
+        
 
-            print('| ' + elem + space + '| ' + str(df.dtypes[elem]) + ' |')
-            print('-------------------------------------')
-        # print(self.df.dtypes)
+    def shape(self):
+        df = self.df
+
+        print(df.shape)
+        print(df.head())
+        print(df.isnull().sum())
+
     def hopkins(self):
         df = self.df
-        self.X = np.array(df[[
-            'LotArea','OverallQual', 'TotRmsAbvGrd', 'GarageCars', 'FullBath'
-        ]])
+        column_names = ['LotArea','OverallQual', 'TotRmsAbvGrd', 'GarageCars', 'FullBath']
+        row_names = ['SalePrice']
+        self.X = np.array(df[column_names])
         X = self.X
-        self.Y = np.array(df[['SalePrice']])
+        self.Y = np.array(df[row_names])
+        
+        Y = self.Y
         random.seed(10000)
         X_scale = sklearn.preprocessing.scale(X)
         hop = pyclustertend.hopkins(X,len(X))
-
-        return hop, X_scale, X
+        
+        df = pd.DataFrame(df, columns=column_names)
+        return hop, X_scale, X, Y, df
 
     # fuzzy c-means algorithms 
     def fuzzy_cMeans(self):
-        hop, X_scale, X = self.hopkins()
+        hop, X_scale, X, Y, df = self.hopkins()
         
         fcm = FCM(n_clusters = 5)
         fcm.fit(X)
@@ -74,7 +65,7 @@ class main(object):
         plt.show()
         
     def clusterNum(self):
-        hop, X_scale, X = self.hopkins()
+        hop, X_scale, X, Y, df = self.hopkins()
         numeroClusters = range(1,11)
         wcss = []
         for i in numeroClusters:
@@ -107,17 +98,59 @@ class main(object):
 
         df_balance['SaleRange'] = df_balance['SaleRange'].astype('category')
         print(df_balance)
+        # print("\n df")
         return df
-
+        
     def trainTest(self):
         df = self.df
-        y = df.pop("SalePrice") #La variable respuesta
-        X = df #El   resto de los datos
-        random.seed(10000)
-        X_train, X_test,y_train, y_test = train_test_split(X, y,test_size=0.3,train_size=0.7)
+        hop, X_scale, X, Y, df = self.hopkins()
+  
 
+        X_train, X_test,y_train, y_test = train_test_split(X, Y, random_state =101, test_size=0.3,train_size=0.7)
+      
+        return X_train, X_test,y_train, y_test
+
+    def treeDepth(self):
+        df = self.df
+        X_train, X_test,y_train, y_test = self.trainTest()
+
+        train_accuracy = []
+        test_accuracy = []
+
+        for depth in range(1, 10):
+            df = tree.DecisionTreeClassifier(max_depth=depth, random_state=10)
+            df.fit(X_train, y_train)
+            
+            train_accuracy.append(df.score(X_train, y_train))
+            test_accuracy.append(df.score(X_test, y_test))
+
+        frame = pd.DataFrame({'max_depth':range(1, 10), 'train_acc':train_accuracy, 'test_acc':test_accuracy})
+        print(frame.head())
+
+        # plt.figure(figsize=(12, 6))
+        # plt.plot(frame['max_depth'], frame['train_acc'], marker='o')
+        # plt.plot(frame['max_depth'], frame['test_acc'], marker='o')
+        # plt.xlabel('Depth of tree')
+        # plt.ylabel('Performance')
+        # plt.legend()
+        # plt.show()
+
+
+        # EL DEPTH ES DE 3
+
+    def decision_tree(self):
+        X_train, X_test,y_train, y_test = self.trainTest()
+        hop, X_scale, X, Y, df = self.hopkins()
+
+        dt = tree.DecisionTreeClassifier(max_depth=3, random_state=10)
+        dt.fit(X_train, y_train)
+
+        feature_names = df.columns
+        tree.export_graphviz(dt, out_file='tree.dot', feature_names=feature_names, class_names=True, max_depth=2)
+        
+        # para correrlo tiene que descargar graphviz
+        # despues -> dot -Tpng tree.dot -o tree.png
     
-
 
 driver = main('../train.csv')
 
@@ -125,4 +158,5 @@ driver = main('../train.csv')
 #print(driver.hopkins()[0])
 #driver.fuzzy_cMeans()
 # print(driver.clusterNum())
-driver.groupBy_ResponseVar()
+driver.decision_tree()
+    
